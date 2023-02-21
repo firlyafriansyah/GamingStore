1d5bec5c6925749428b56ad40ab08615830be6c9
Firly Afriansyah <70530555+firlyafriansyah@users.noreply.github.com>
2023-02-21 11:57:06 +0700

    Add head for sign up success page

--- pages/signup-success.tsx
+++ pages/signup-success.tsx
@@ -4,15 +4,18 @@ import Image from 'next/image'
 
 export default function SignupSuccess() {
   return (
-    <div className={styles.container}>
-      <div className={styles['illustration-wrapper']}>
-        <Image src='/Illustration/wallet.svg' fill alt="wallet-icon" priority />
+    <>
+      <HeadCustom title="Sign Up Success - Gaming Store" />
+      <div className={styles.container}>
+        <div className={styles['illustration-wrapper']}>
+          <Image src='/Illustration/wallet.svg' fill alt="wallet-icon" priority />
+        </div>
+        <h1 className={styles.title}>Well Done!</h1>
+        <p className={styles.subtitle}>Kamu sudah bisa melakukan top up<br/>dan menjadi pemenang!</p>
+        <div className={styles['button-wrapper']}>
+          <CustomButton text='Top Up' width='100%' height={50} bgColor='#4D17E2' color='#ffffff' navigation='/' />
+        </div>
       </div>
-      <h1 className={styles.title}>Well Done!</h1>
-      <p className={styles.subtitle}>Kamu sudah bisa melakukan top up<br/>dan menjadi pemenang!</p>
-      <div className={styles['button-wrapper']}>
-        <CustomButton text='Top Up' width='100%' height={50} bgColor='#4D17E2' color='#ffffff' navigation='/' />
-      </div>
-    </div>
+    </>
   )
 }
